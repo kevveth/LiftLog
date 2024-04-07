11acc42887156134e620fc07167938c757ba82cf
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-04-06 23:00:58 -0700

    Initial Commit

---
+++ LiftLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiftLog
+//
+//  Created by Kenneth Oliver Rathbun on 4/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiftLog/LiftLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftLogApp.swift
+//  LiftLog
+//
+//  Created by Kenneth Oliver Rathbun on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LiftLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
