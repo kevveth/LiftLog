e00855ad88714c17be05e15c4fb4076b710b4ad0
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-04-10 16:00:33 -0700

    Created PathStore to store navigation data

---
+++ PathStore.swift
@@ -0,0 +1,46 @@
+//
+//  PathStore.swift
+//  LiftLog
+//
+//  Created by Kenneth Oliver Rathbun on 4/10/24.
+//
+
+import Foundation
+import SwiftUI
+import Observation
+
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init() {
+        // Retrieve data from save path
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        // If still here, start with an empty path
+        path = NavigationPath()
+    }
+    
+    // Save nav data to save path
+    func save() {
+        guard let representation = path.codable else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
